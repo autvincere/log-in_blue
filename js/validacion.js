63ff04d8cd67f244fe0e0bb6099e26121dcd14d3
autvincere <aut.vincere@gmail.com>
2016-02-16 17:39:47 -0300

    all

---
+++ js/validacion.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $("form").submit(function (e) {
+        e.preventDefault();
+//        var expr = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+        var expr1 = /^[a-zA-Z]*$/
+        var usuario = $("input[name=usuario]").val();
+        var contrasena = $("input[name=contrasena]").val();
+        //console.log(usuario, contrasena);
+        if (usuario == "" || !expr1.test(usuario)) {
+          $("div#wrapper_usuario").effect("shake", {direction: "left", distance:8,times:3},500);
+            $("div#error_usuario").html("<h4>error de usuario</h4>");
+//            $("<h4>error usuario</h4>").appendTo("div#error_usuario").hide().fadeIn(1000);
+//                var duplicado = $("<h4>error usuario</h4>").length;
+//               parseInt (duplicado);
+//                    console.log(duplicado);
+//                if( duplicado == 0){
+//                        $("<h4>error usuario</h4>").remove();
+//                };
+            return false;
+        } else {
+            $("div#error_usuario").remove();
+            console.log("usuario correcto")
+        };
+        if (contrasena == "") {
+            $("div#wrapper_contrasena").effect("shake", {direction: "left", distance:8,times:3},500);
+            $("div#error_contrasena").html("<h4>error de contraseña</h4>");
+            return false;
+        } else {
+            $("div#error_contrasena").remove();
+            console.log("contraseña correcta")
+        };
+    });
+
+
+});
